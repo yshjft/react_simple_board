dd7f3f762b502395bbd748880b14aad950924716
yshjft <jericoh@naver.com>
2020-08-19 01:44:15 +0900

    edit date form from Write.jsx

--- src/components/Main/Write/Write.jsx
+++ src/components/Main/Write/Write.jsx
@@ -3,12 +3,20 @@ import React, {Component} from "react";
 import "./Write.css";
 
 class Write extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            year : new Date().getFullYear(),
+            month : new Date().getMonth()+1,
+            date : new Date().getDate(),
+        };
+    }
     render(){
         return(
             <div className="Main">
-                <div className="writeForm">
+                <form id="writeForm" className="writeForm">
                     <div className="form">
-                        <input type="txt" placeholder="Title" className="input title" required autoFocus></input>
+                        <input type="txt" placeholder="Title" className="input title" required></input>
                     </div>
                     <div className="form">
                         <input type="txt" placeholder="Subtitle" className="input subTitle"></input>
@@ -17,7 +25,7 @@ class Write extends Component{
                         <input type="txt" placeholder="Writer" className="input writer" required></input>
                     </div>
                     <div className="form">
-                        <input type="date" className="input date"></input>
+                        <input value={this.state.year+"."+this.state.month+"."+this.state.date} className="input date" readOnly></input>
                     </div>
                     <div className="form">
                         <textarea placeholder="write something" className="write" required></textarea>
@@ -28,7 +36,7 @@ class Write extends Component{
                     <div className="form">
                         <input type="submit" value="SUBMIT" className="submitBtn"></input>
                     </div>
-                </div>
+                </form>
             </div>
         );
     }
