959aa6e9bed72c729587d86dd2397bc8e47bfff4
yshjft <jericoh@naver.com>
2020-08-31 01:27:39 +0900

    Updates

---
+++ precache-manifest.ef5b94aaa5d22c5d605de3d625a2a640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0cf71475f7f406566fbf81d52af3cef",
+    "url": "/react_simple_board/index.html"
+  },
+  {
+    "revision": "5f3ce1be0d27991a6748",
+    "url": "/react_simple_board/static/css/main.ddfb7f50.chunk.css"
+  },
+  {
+    "revision": "0ce07e059031eea8a4db",
+    "url": "/react_simple_board/static/js/2.041a1be1.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/react_simple_board/static/js/2.041a1be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f3ce1be0d27991a6748",
+    "url": "/react_simple_board/static/js/main.4a104349.chunk.js"
+  },
+  {
+    "revision": "8cdf6f767c56b2f267d3",
+    "url": "/react_simple_board/static/js/runtime-main.a1f02f57.js"
+  }
+]);
